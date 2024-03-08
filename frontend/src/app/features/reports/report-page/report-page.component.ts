c60a95a86814401cd839991307b777d1dd8081aa
Nichifor Dragos <dragos.nichifor@stud.ubbcluj.ro>
2024-03-08 02:01:14 +0200

    ✨ Redesign report details page

--- frontend/src/app/features/reports/report-page/report-page.component.ts
+++ frontend/src/app/features/reports/report-page/report-page.component.ts
@@ -12,6 +12,15 @@ import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import { MatChipsModule } from '@angular/material/chips';
 import { DateConverterModule } from '$shared/date-converter';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { TinyEditorModule } from '$shared/tiny-editor';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-report-page',
@@ -26,11 +35,22 @@ import { DateConverterModule } from '$shared/date-converter';
     CommonModule,
     MatChipsModule,
     DateConverterModule,
+    MatInputModule,
+    MatFormFieldModule,
+    TinyEditorModule,
+    ReactiveFormsModule,
   ],
 })
 export class ReportPageComponent implements AfterViewInit {
   @Input() report!: ReportDetails;
+
   reportService = inject(ReportService);
+
+  replyForm = inject(FormBuilder).group({
+    reply: ['', Validators.required],
+  });
+
+  reply: boolean = false;
 
   async ngAfterViewInit() {
     if (this.report.isRead) {
@@ -40,5 +60,11 @@ export class ReportPageComponent implements AfterViewInit {
     await this.reportService.reportsReadPatchAsync({
       body: { id: this.report.id },
     });
+  }
+
+  sendReply(newReply: typeof this.replyForm.value) {
+    this.reply = false;
+
+    console.log(newReply);
   }
 }
--- frontend/src/app/shared/date-converter/date-converter.pipe.ts
+++ frontend/src/app/shared/date-converter/date-converter.pipe.ts
@@ -5,15 +5,68 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateFormatPipe implements PipeTransform {
   transform(value: string): string {
-    if (!value) return '';
+    if (!value) {
+      return '';
+    }
+
     const date = new Date(value);
+
     const year = date.getFullYear();
-    const month = this.padNumber(date.getMonth() + 1);
-    const day = this.padNumber(date.getDate());
-    return `${year}/${month}/${day}`;
+    const month = this.getMonth(date.getMonth() + 1);
+    const day = date.getDate();
+
+    return `${month} ${day} ${year}`;
   }
 
-  private padNumber(num: number): string {
-    return num < 10 ? '0' + num : '' + num;
+  private getMonth(month: number) {
+    if (month === 1) {
+      return 'Jan.';
+    }
+
+    if (month === 2) {
+      return 'Feb.';
+    }
+
+    if (month === 3) {
+      return 'Mar.';
+    }
+
+    if (month === 4) {
+      return 'Apr.';
+    }
+
+    if (month === 5) {
+      return 'May';
+    }
+
+    if (month === 6) {
+      return 'Jun.';
+    }
+
+    if (month === 1) {
+      return 'Jul.';
+    }
+
+    if (month === 1) {
+      return 'Aug.';
+    }
+
+    if (month === 1) {
+      return 'Sep.';
+    }
+
+    if (month === 1) {
+      return 'Oct.';
+    }
+
+    if (month === 1) {
+      return 'Nov.';
+    }
+
+    if (month === 1) {
+      return 'Dec.';
+    }
+
+    return '';
   }
 }
