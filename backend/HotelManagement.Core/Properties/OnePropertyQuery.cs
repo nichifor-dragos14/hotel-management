653d1ceabfb523a68adb4f7d5397011a1b8e004a
Nichifor Dragos <dragos.nichifor@stud.ubbcluj.ro>
2024-04-06 20:32:02 +0300

    🐛 Fixed get one property query

--- backend/HotelManagement.Core/Properties/OnePropertyQuery.cs
+++ backend/HotelManagement.Core/Properties/OnePropertyQuery.cs
@@ -73,68 +73,83 @@ internal class OneHotelQueryHandler(
 {
     public async Task<PropertyDetails> ExecuteAsync(
         OnePropertyQuery query,
-        CancellationToken cancellationToken
-    )
+        CancellationToken cancellationToken)
     {
-        return
-            (from property in facade.Of<Property>()
-             where property.Id == query.Id
-             select new PropertyDetails(
-                 property.Id,
-                 property.Name,
-                 property.Type,
-                 property.Description,
-                 property.Location,
-                 property.Email,
-                 property.PhoneNumber,
-                 property.Rating,
-                 property.Reviews.Any() ? property.Reviews.Select(r => r.Rating).Average() : 0,
-                 property.CreatedOn,
-                 property.UpdatedOn,
-                 property.HasFreeWiFi,
-                 property.HasParking,
-                 property.HasPool,
-                 property.HasRestaurant,
-                 property.HasFitnessCenter,
-                 property.HasRoomService,
-                 property.HasPetFriendlyPolicy,
-                 property.HasBreakfast,
-                 property.HasFreeCancellation,
-                 property.PrepaymentNeeded,
-                 property.Rooms.Select(r => new RoomPropertyDetails(
-                     r.Id,
-                     r.Number,
-                     r.Type,
-                     r.Price,
-                     r.AdultCapacity,
-                     r.ChildrenCapacity,
-                     r.HasPrivateBathroom,
-                     r.HasTowels,
-                     r.HasHairdryer,
-                     r.HasAirConditioning,
-                     r.HasBalcony,
-                     r.HasRefrigerator,
-                     r.HasSeaView,
-                     r.CreatedOn,
-                     r.UpdatedOn
-                     )
-                 ),
-                 property.Reviews.Select(r => new ReviewPropertyDetails(
-                     r.Id,
-                     r.Title,
-                     r.Description,
-                     r.Rating,
-                     r.CreatedOn,
-                     r.UpdatedOn,
-                     new ReviewUserPropertyDetails(
-                         r.User.Id,
-                         r.User.FirstName,
-                         r.User.LastName,
-                         r.User.Nationality
-                         ) ?? null
-                     )
-                 )
-              )
-            ).FirstOrDefault();
+        var propertyDetails = (from p in facade.Of<Property>()
+                               where p.Id == query.Id
+                               select new
+                               {
+                                   Property = p,
+                                   Rooms = p.Rooms.OrderBy(r => r.CreatedOn).Take(3),
+                                   Reviews = p.Reviews.OrderBy(r => r.CreatedOn).Take(5).Select(r => new
+                                   {
+                                       Review = r,
+                                       r.User
+                                   })
+                               })
+                               .FirstOrDefault();
+
+        if (propertyDetails == null) return null;
+
+        var roomsDetails = propertyDetails.Rooms.Select(r => new RoomPropertyDetails(
+            r.Id,
+            r.Number,
+            r.Type,
+            r.Price,
+            r.AdultCapacity,
+            r.ChildrenCapacity,
+            r.HasPrivateBathroom,
+            r.HasTowels,
+            r.HasHairdryer,
+            r.HasAirConditioning,
+            r.HasBalcony,
+            r.HasRefrigerator,
+            r.HasSeaView,
+            r.CreatedOn,
+            r.UpdatedOn
+        )).ToList();
+
+        var reviewsDetails = propertyDetails.Reviews.Select(r => new ReviewPropertyDetails(
+            r.Review.Id,
+            r.Review.Title,
+            r.Review.Description,
+            r.Review.Rating,
+            r.Review.CreatedOn,
+            r.Review.UpdatedOn,
+            r.User == null ? null : new ReviewUserPropertyDetails(
+                r.User.Id,
+                r.User.FirstName,
+                r.User.LastName,
+                r.User.Nationality
+            )
+        )).ToList();
+
+        var property = propertyDetails.Property;
+
+        return new PropertyDetails(
+            property.Id,
+            property.Name,
+            property.Type,
+            property.Description,
+            property.Location,
+            property.Email,
+            property.PhoneNumber,
+            property.Rating,
+            property.Reviews.Count != 0 ? property.Reviews.Average(r => r.Rating) : 0,
+            property.CreatedOn,
+            property.UpdatedOn,
+            property.HasFreeWiFi,
+            property.HasParking,
+            property.HasPool,
+            property.HasRestaurant,
+            property.HasFitnessCenter,
+            property.HasRoomService,
+            property.HasPetFriendlyPolicy,
+            property.HasBreakfast,
+            property.HasFreeCancellation,
+            property.PrepaymentNeeded,
+            roomsDetails,
+            reviewsDetails
+        );
     }
 }
