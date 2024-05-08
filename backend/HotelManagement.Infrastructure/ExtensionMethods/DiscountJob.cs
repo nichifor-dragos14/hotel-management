267105b10f2eaf59bdfa6efc9327c2d0ccba016d
Nichifor Dragos <dragos.nichifor@stud.ubbcluj.ro>
2024-05-08 10:51:34 +0300

    ✨ Only send mails if user wants to

--- backend/HotelManagement.Infrastructure/ExtensionMethods/DiscountJob.cs
+++ backend/HotelManagement.Infrastructure/ExtensionMethods/DiscountJob.cs
@@ -75,7 +75,10 @@ public class DiscountJob(IQueryFacade queryFacade, IUnitOfWork unitOfWork, IEmai
 
             await unitOfWork.SaveChangesAsync(CancellationToken.None);
 
-            await emailService.SendDiscountsNotificationOnEmail(user.FirstName, user.LastName, user.Email, startDate, endDate);
+            if(user.SendOffersOnEmail)
+            {
+                await emailService.SendDiscountsNotificationOnEmail(user.FirstName, user.LastName, user.Email, startDate, endDate);
+            }
         }
 
         Console.WriteLine("Discount job executed at: " + DateTime.Now);
